ad6414db78a2c25e8eeb75b11b6e4d6f34b6239e
Edgar Figueroa <edgarefigueroa@outlook.com>
2022-06-26 09:31:23 -0400

    sdlc

---
+++ sdlc.js
@@ -0,0 +1,33 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+
+//1. Planning
+//2. Analysis of Requirements
+//3. Design 
+//4. Implementation
+//5. Testing & Integration
+//6. Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "This is the second step, in which includes the gathering of all the specific details required for new system as well determining the first ideas for prototypes."
+const design = "This is the third step, in which will outline the details for overall application." 
+const implementation = "This is the fourth step, in which is the part where developer actually write code and build the application according to the earlier design document." 
+const testingIntegration = "This is the fifth step, in which developers will go over their software noting any bugs or defects that need to be tracked, fixed, and later retested. System will be integrated into its enviroment and eventually installed."
+const maintain = "This is the sixth step, in which developers begin practicing any activities required to handle issues reported by end-users. Developers are responsible for implementing any changes that the software might need after deployment."
